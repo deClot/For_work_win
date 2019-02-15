86d2b46f9896d30bbc9123d8ef1a2d4e31b22b1c
Nadezda Kashirina <refenement@mail.ru>
2019-02-15 13:26:55 +0700

    Separate calculations deltas and filling empties

--- check_series_transitions.py
+++ check_series_transitions.py
@@ -5,87 +5,55 @@ def delta_full_2(branch_1, branch_0, delta_type):
     elif delta_type == 2:
         lenght, index = 6, 5
 
-    print('delta_full_2','delta_type=',delta_type)
-    print('len1=',len(branch_1), 'len0=', len(branch_0),'index=', index )
-    
     # if lenght of one of branch if less than index(list[index] to calculate delta)
     # then do nothing
     if len(branch_1) <= index or len(branch_0) <= index:
         return None
 
-    print('len1=',len(branch_1),'len=', lenght)
-    
     # if list does not include delta, then add delta to list 
     if len(branch_1) == lenght:
-        print('branch_1[index]=',branch_1[index],\
-              ' branch_0[index]=',branch_0[index])
         branch_1.append(branch_1[index]-branch_0[index])
-        print (branch_1, branch_0)
-    else:
-        return None
 
 
-def delta_empty_full(empty, full, delta, delta_type):
-    if delta_type == 1:
-        lenght = 5
-        addition = 0
-    elif delta_type == 2:
-        lenght = 6
-        addition = full[5]
-
-    print ('delta_empty_full', empty, full)
-        
-    if empty[2] < full[2]:  # if empty-full sequence
-        empty[0] = full[0] - delta - addition # Calculate tr for empty
-        
-    elif empty[2] > full[2]: # if full-empty sequence
-        empty[0] = full[0] + delta + addition # Calculate tr for empty
-
-    else:
-        raise ValueError('2 transitions have the same valus of J')
-
-    empty.append(delta)
-    # Write info for other
-    if len(full) == lenght:
-        full.append(delta)
-
-
-
-    print (empty, full)
-
-    
 def calculate_delta(branch, i, delta_type):
     if delta_type == 1:
         delta_index, lenght = 5, 6
     elif delta_type == 2:
         delta_index, lenght = 6, 7
 
-    print (branch[i], branch[i-1],'delta_type=', delta_type)
-    print ('len1=',len(branch[i]),'len0=',len(branch[i-1]),'lenght=',lenght)
-
     #if delta are exsists in both cases
     if len(branch[i]) == lenght and len(branch[i-1]) == lenght:
-        print ('Delta already calculated')
         return None
     
     # for one or for both transitions do not exist info
     if branch[i][1] != None and branch[i-1][1] != None:
         delta_full_2(branch[i], branch[i-1], delta_type=delta_type)
-
-    #
-    elif len(branch[i]) == lenght or len(branch[i-1]) == lenght:
-        if len(branch[i-1]) == lenght:
-            delta_empty_full(branch[i], branch[i-1], branch[i-1][delta_index],delta_type)
-        elif len(branch[i]) == lenght:
-            delta_empty_full(branch[i-1], branch[i], branch[i][delta_index],delta_type)
     else:
-        print('Move forward')
-        calculate_delta(branch, i+1, delta_type)
-        if len(branch[i+1]) == lenght:
-            print ('Copy delta from ', branch[i+1])
-            delta = branch[i+1][delta_index]
-            delta_empty_full(branch[i-1], branch[i], delta, delta_type)
-   
+        return None
+
+
+def fill_empties_delta (branch, i):
+    if len(branch[i]) != 7:
+        try:
+            delta1, delta2 = fill_empties_delta(branch, i+1)
+        except IndexError:
+            return None,None
+        if delta1 == None:
+            return None,None
+        
+        if len(branch[i]) == 6:
+            delta1 = branch[i][5]
+            branch[i].append(delta2)
+            return delta1, delta2
+
+        if len(branch[i]) == 5:
+            branch[i].append(delta1-delta2)
+            branch[i].append(delta2)
+            return delta1-delta2, delta2
+    else:
+        return branch[i][5], branch[i][6]
+
+
 def check_series_transitions_delta1(name):
     '''
     Check transitions; if there are None - calculate possibale value;
@@ -101,16 +69,18 @@ def check_series_transitions_delta1(name):
             continue
         J, Ka = branch[0][2],branch[0][3]
 
+        # calculate delta1 for excisting transitions
         for i in range(1, len(branch)):
-            print ('Start calculation delta 1')
             calculate_delta(branch, i, delta_type=1)
-            print ('After calculation')
-            print (branch[i], branch[i-1])
-            #input()
-
+            
+        # calculate delta2 for excisting transitions
         for i in range(1, len(branch)):
             calculate_delta(branch, i, delta_type=2)
 
+        # calculate delta1 and delta 2 for empty transitions
+        for i in range(0, len(branch)):
+            fill_empties_delta(branch, i)
+            input()
         '''
         if J > Ka:
             for i in range(J,Ka,-1):
