2a7926a239594a9d46aeb24eea299cc2a9b9366a
Nadezda Kashirina <refenement@mail.ru>
2019-02-26 16:13:40 +0700

    Switch from module to main

--- module.py
+++ module.py
@@ -32,8 +32,6 @@ def main_function(src):
            _,J0,Ka0,Kc0,*_=str1 
            J0, Ka0, Kc0 =int(J0), int(Ka0), int(Kc0)
 
-           transitions = []
-
            Up_State1 = Transition(J0, Ka0, Kc0)
            Up = Transition(J0, Ka0, Kc0)
            count += 1  #+1 for first series
@@ -50,14 +48,12 @@ def main_function(src):
            J0, Ka0, Kc0 =int(J0), int(Ka0), int(Kc0)
 
            #We registred next energy,so write info about previous energy in file
-           print(counter)
            write_search(Up,file_search)
            file_search.write('   '+str1)
 
            Up = Transition(J0, Ka0, Kc0)
            # stay in same series
            if abs(ref.J-J0) == abs(ref.Kc-Kc0) and ref.Ka == Ka0:
-               counter += 1         
                continue
            #change series
            else:
@@ -71,13 +67,10 @@ def main_function(src):
                        ref = Up_State2
                    elif ref == Up_State2:
                        ref = Up_State1
-                       counter += 1
-           
-                        
        else:
            separate_transitions.Separate_transitions(J0,Ka0,Kc0,str1,\
-                                                     transitions,ref,Up)
-
+                                                     ref,Up)
+    file_ini.close()
     file2 = open('RESULTS', 'w')
 
     if count > 1:
@@ -93,90 +86,7 @@ def main_function(src):
         for branches in (branches_a, branches_b):
             write_predictions(branches, file2)
             
-
-        
-                
-        '''file2.write('%3d%3d%3d%12.5f%7.3f\n'%(branch[i][2],branch[i][3],
-                                                          branch[i][4],branch[i][0],
-                                                          branch[i][1]))
-        '''
-                
-        '''
-
-    for name in name_list:
-       file2.write('\n\n!!!!!!!!!!!!!\n')
-       file2.write('!!!--R Branch--!!!\n')
-       for i in range(len(name.R_Tr)):
-           if i == 0:
-               file2.write('%3d%3d%3d%12.5f %7.3f\n' % (name.R_Tr[i][2],name.R_Tr[i][3],name.R_Tr[i][4],\
-                                                        name.R_Tr[i][0], name.R_Tr[i][1]))
-           elif i==1:
-               file2.write('%3d%3d%3d%12.5f %7.3f| %9.5f\n' % (name.R_Tr[i][2],name.R_Tr[i][3],name.R_Tr[i][4],\
-                                                              name.R_Tr[i][0], name.R_Tr[i][1],\
-                                                              name.R_Tr[i][0]-name.R_Tr[i-1][0]))
-           else:
-               file2.write('%3d%3d%3d%12.5f %7.3f| %9.5f %9.5f\n' % (name.R_Tr[i][2],name.R_Tr[i][3],name.R_Tr[i][4],\
-                                                              name.R_Tr[i][0], name.R_Tr[i][1],\
-                                                              name.R_Tr[i][0]-name.R_Tr[i-1][0],\
-                                                              name.R_Tr[i][0]-name.R_Tr[i-1][0]-\
-                                                              name.R_Tr[i-1][0]+name.R_Tr[i-2][0] ))
-
-       file2.write('\n!!!--Q Branch--!!!\n')
-       for i in range(len(name.Q_Tr)):
-           if i == 0:
-               file2.write('%3d%3d%3d%12.5f %7.3f\n' % (name.Q_Tr[i][2],name.Q_Tr[i][3],name.Q_Tr[i][4],\
-                                                        name.Q_Tr[i][0],name.Q_Tr[i][1]))
-           elif i==1:
-               file2.write('%3d%3d%3d%12.5f %7.3f| %9.5f\n' % (name.Q_Tr[i][2],name.Q_Tr[i][3],name.Q_Tr[i][4],\
-                                                              name.Q_Tr[i][0], name.Q_Tr[i][1],\
-                                                     name.Q_Tr[i][0]-name.Q_Tr[i-1][0] ))
-           else:
-               file2.write('%3d%3d%3d%12.5f %7.3f| %9.5f %9.5f\n' % (name.Q_Tr[i][2],name.Q_Tr[i][3],name.Q_Tr[i][4],\
-                                                              name.Q_Tr[i][0], name.Q_Tr[i][1],\
-                                                              name.Q_Tr[i][0]-name.Q_Tr[i-1][0],\
-                                                              name.Q_Tr[i][0]-name.Q_Tr[i-1][0]-\
-                                                              name.Q_Tr[i-1][0]+name.Q_Tr[i-2][0] ))
-
-       file2.write('\n!!!--P Branch--!!!\n')
-       for i in range(len(name.P_Tr)):
-           if i == 0:
-               file2.write('%3d%3d%3d%12.5f %7.3f\n' % (name.P_Tr[i][2],name.P_Tr[i][3],name.P_Tr[i][4],\
-                                                    name.P_Tr[i][0], name.P_Tr[i][1]))
-           elif i==1:
-               file2.write('%3d%3d%3d%12.5f %7.3f| %9.5f\n' % (name.P_Tr[i][2],name.P_Tr[i][3],name.P_Tr[i][4],\
-                                                              name.P_Tr[i][0], name.P_Tr[i][1],\
-                                                              name.P_Tr[i][0]-name.P_Tr[i-1][0] ))
-           else:
-               file2.write('%3d%3d%3d%12.5f %7.3f| %9.5f %9.5f\n' % (name.P_Tr[i][2],name.P_Tr[i][3],name.P_Tr[i][4],\
-                                                              name.P_Tr[i][0], name.P_Tr[i][1],\
-                                                              name.P_Tr[i][0]-name.P_Tr[i-1][0],\
-                                                              name.P_Tr[i][0]-name.P_Tr[i-1][0]-\
-                                                              name.P_Tr[i-1][0]+name.P_Tr[i-2][0] ))
-
-    for attribute in [name.P_Tr_f1,name.P_Tr_f2,name.R_Tr_f1,name.R_Tr_f2,\
-                      name.Q_Tr_f1,name.Q_Tr_f2]:
-       if len(attribute)!=0:
-           file2.write('\n!!!--Forbidden Branch--!!!\n')
-           for i in range(len(attribute)):
-               if i == 0:
-                   file2.write('%3d%3d%3d%12.5f %7.3f\n' % (attribute[i][2],attribute[i][3],attribute[i][4],\
-                                                            attribute[i][0], attribute[i][1]))
-               elif i==1:
-                   file2.write('%3d%3d%3d%12.5f %7.3f| %9.5f\n' % (attribute[i][2],attribute[i][3],attribute[i][4],\
-                                                                  attribute[i][0], attribute[i][1],\
-                                                                  attribute[i][0]-attribute[i-1][0]))
-               else:
-                   file2.write('%3d%3d%3d%12.5f %7.3f| %9.5f %9.5f\n' % (attribute[i][2],attribute[i][3],attribute[i][4],\
-                                                                         attribute[i][0], attribute[i][1],\
-                                                                         attribute[i][0]-attribute[i-1][0],\
-                                                                         attribute[i][0]-attribute[i-1][0]\
-                                                                         -attribute[i-1][0]+attribute[i-2][0] ))
-
-    '''
-
-    
     file2.close()
+    file_search.close()
 
-
-
-main_function('In/v12.RESULT')
+#main_function('In/v12.RESULT')
--- separate_transitions.py
+++ separate_transitions.py
@@ -2,7 +2,7 @@ import re
 
 import check_branch
 
-def Separate_transitions(J0,Ka0,Kc0,str1,transitions,Tr,Up):
+def Separate_transitions(J0,Ka0,Kc0,str1,Tr,Up):
     #str1 = '3  0  3  3663.43740 13.9  3710.59730    5  0  5  3597.93318 16.5  3710.59711 -r'
     
     template_comment1 = r'\A[a-zA-Z-!+]+' #comment in the beginin of line
@@ -19,7 +19,6 @@ def Separate_transitions(J0,Ka0,Kc0,str1,transitions,Tr,Up):
     template_transition = r'\d+\s+\d+\s+\d+\s+\d+\.\d+\s+\d+\.\d+\s+\d+.\d+'
     blocks = re.findall(template_transition, str1)
     for block in blocks:
-        transitions.append(block)
         J,Ka,Kc,Trans,I,E = block.split()
         check_branch.check_branch (J0,J,Ka0,Ka,Kc0,Kc,Trans,I,E,Tr,Up)
     return 1
